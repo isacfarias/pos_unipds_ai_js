d76af20756073b00c88faa3b290e84f5dc0a1439
Isac Farias <isac.farias.21@gmail.com>
2026-06-14 14:01:36 -0400

    normalizando dados

--- exemplo-01-ecommerce-recomendations/src/workers/modelTrainingWorker.js
+++ exemplo-01-ecommerce-recomendations/src/workers/modelTrainingWorker.js
@@ -3,12 +3,19 @@ import { workerEvents } from '../events/constants.js';
 
 console.log('Model training worker initialized');
 let _globalCtx = {};
+const WEIGHTS = {
+    category: 0.4,
+    color: 0.3,
+    price: 0.2,
+    age: 0.1,
+};
 
-const normalize = (value, min, max) => (value - min) / ((max - min) || 1 )
-
-function makeContext(catalog, users) {
-    const ages   = users.map(u => u.age)
-    const prices = catalog.map(p => p.price)
+// Formula: (val - min) / (max - min)
+// Example: price=129.99, minPrice=39.99, maxPrice=199.99 → 0.56
+const normalize = (value, min, max) => (value - min) / ((max - min) || 1)
+function makeContext(products, users) {
+    const ages = users.map(u => u.age)
+    const prices = products.map(p => p.price)
 
     const minAge = Math.min(...ages)
     const maxAge = Math.max(...ages)
@@ -16,25 +23,20 @@ function makeContext(catalog, users) {
     const minPrice = Math.min(...prices)
     const maxPrice = Math.max(...prices)
 
-    const colors     = [...new Set(catalog.map(p => p.color))]
-    const categories = [...new Set(catalog.map(p => p.category))]
+    const colors = [...new Set(products.map(p => p.color))]
+    const categories = [...new Set(products.map(p => p.category))]
 
-    const colorIndex = Object.entries(
-        colors.map(( color, index ) => {
+    const colorsIndex = Object.fromEntries(
+        colors.map((color, index) => {
             return [color, index]
-        })
-    )
-
-    const categoriesIndex = Object.entries(
-        colors.map(( category, index ) => {
+        }))
+    const categoriesIndex = Object.fromEntries(
+        categories.map((category, index) => {
             return [category, index]
-        })
-    )    
+        }))
 
-    /**
-     * computar a média de idade do compradores pro produto
-     * ajuda a personalizar a resposta
-     */
+    // Computar a média de idade dos comprados por produto
+    // (ajuda a personalizar)
     const midAge = (minAge + maxAge) / 2
     const ageSums = {}
     const ageCounts = {}
@@ -44,41 +46,84 @@ function makeContext(catalog, users) {
             ageSums[p.name] = (ageSums[p.name] || 0) + user.age
             ageCounts[p.name] = (ageCounts[p.name] || 0) + 1
         })
-    });
-
+    })
 
     const productAvgAgeNorm = Object.fromEntries(
-        catalog.map(product => {
+        products.map(product => {
             const avg = ageCounts[product.name] ?
-             ageSums[product.name] / ageCounts[product.name] :
-            midAge
+                ageSums[product.name] / ageCounts[product.name] :
+                midAge
 
             return [product.name, normalize(avg, minAge, maxAge)]
         })
     )
 
-    debugger
-
     return {
-        catalog,
+        products,
         users,
-        colorIndex,
+        colorsIndex,
         categoriesIndex,
+        productAvgAgeNorm,
         minAge,
         maxAge,
         minPrice,
         maxPrice,
         numCategories: categories.length,
         numColors: colors.length,
+        // price + age + colors + categories
         dimentions: 2 + categories.length + colors.length
     }
+}
+const oneHotWeighted = (index, length, weight) =>
+    tf.oneHot(index, length).cast('float32').mul(weight)
+
+function encodeProduct(product, context) {
+    // normalizando dados para ficar de 0 a 1 e
+    // aplicar o peso na recomendação
+    const price = tf.tensor1d([
+        normalize(
+            product.price,
+            context.minPrice,
+            context.maxPrice
+        ) * WEIGHTS.price
+    ])
+
+    const age = tf.tensor1d([
+        (
+            context.productAvgAgeNorm[product.name] ?? 0.5
+        ) * WEIGHTS.age
+    ])
+
+    const category = oneHotWeighted(
+        context.categoriesIndex[product.category],
+        context.numCategories,
+        WEIGHTS.category
+    )
+
+    const color = oneHotWeighted(
+        context.colorsIndex[product.color],
+        context.numColors,
+        WEIGHTS.color
+    )
+
+    return tf.concat1d(
+        [price, age, category, color]
+    )
 }
 
 async function trainModel({ users }) {
     console.log('Training model with users:', users)
     postMessage({ type: workerEvents.progressUpdate, progress: { progress: 50 } });
-    const catalog = await (await fetch('/data/products.json')).json()
-    const context = makeContext(catalog, users)
+    const products = await (await fetch('/data/products.json')).json()
+    const context = makeContext(products, users)
+    context.productVectors = products.map(product => {
+         return {
+            name: product.name,
+            meta: { ...product },
+            vector: encodeProduct(product, context).dataSync()
+        }
+    })
+   
     postMessage({
         type: workerEvents.trainingLog,
         epoch: 1,
@@ -90,9 +135,8 @@ async function trainModel({ users }) {
         postMessage({ type: workerEvents.progressUpdate, progress: { progress: 100 } });
         postMessage({ type: workerEvents.trainingComplete });
     }, 1000);
-
-
 }
+
 function recommend(user, ctx) {
     console.log('will recommend for user:', user)
     // postMessage({
@@ -101,7 +145,6 @@ function recommend(user, ctx) {
     //     recommendations: []
     // });
 }
-
 
 const handlers = {
     [workerEvents.trainModel]: trainModel,
