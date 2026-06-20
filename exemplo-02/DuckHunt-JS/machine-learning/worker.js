558d4e637d06e1797b0b38ed7d65d4189ec56fcf
Isac Farias <isac.farias.21@gmail.com>
2026-06-19 21:13:48 -0400

    implementação para o jogo rodar direto

--- exemplo-02/DuckHunt-JS/machine-learning/worker.js
+++ exemplo-02/DuckHunt-JS/machine-learning/worker.js
@@ -3,6 +3,7 @@ importScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest');
 const MODEL_PATH = `yolov5n_web_model/model.json`;
 const LABELS_PATH = `yolov5n_web_model/labels.json`;
 const INPUT_MODEL_DIMENTIONS = 640
+const CLASS_THRESHOLD = 0.35;
 
 let _labels = []
 let _model = null
@@ -18,7 +19,6 @@ async function loadModelAndLabels() {
     tf.dispose(dummyInput)
 
     postMessage({ type: 'model-loaded' })
-
 }
 
 /**
@@ -66,6 +66,34 @@ async function runInference(tensor) {
         classes: classesData
     }
 }
+
+function* processPrediction({ boxes, scores, classes }, width, height) {
+    for (let index = 0; index < scores.length; index++) {
+        
+        if (scores[index] < CLASS_THRESHOLD) continue;
+
+        const label = _labels[classes[index]]
+        if (label !== 'kite') continue
+
+        let [x1, y1, x2, y2] = boxes.slice(index * 4, (index +1 ) * 4)
+        x1 *= width
+        x2 *= width
+        y1 *= height
+        y2 *= height
+
+        const boxWidth = x2 - x1
+        const boxHeight = y2 - y1
+        const centerX = x1 + boxWidth / 2
+        const centerY = y1 + boxHeight / 2
+
+        yield {
+            x: centerX,
+            y: centerY,
+            score: (scores[index] * 100).toFixed(2)
+        }
+    }
+}
+
 loadModelAndLabels()
 
 self.onmessage = async ({ data }) => {
@@ -74,18 +102,14 @@ self.onmessage = async ({ data }) => {
 
     const input = preprocessImage(data.image)
     const { width, height } = data.image
-
     const inferenceResults = await runInference(input)
-
-    debugger
-    postMessage({
-        type: 'prediction',
-        x: 400,
-        y: 400,
-        score: 0
-    });
-
-
+    
+    for (const prediction of processPrediction(inferenceResults, width, height)) {
+        postMessage({
+            type: 'prediction',
+            ...prediction
+        });
+    }
 };
 
 console.log('🧠 YOLOv5n Web Worker initialized');
